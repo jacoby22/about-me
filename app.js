74bbbb680ee5fb5bddc46aec2ca2a96d9bf9a809
Nick Jacoby <nickjacoby22@gmail.com>
2016-05-26 20:41:51 -0700

    updated comments and if statement to now check against correct answers stored in an array

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 //lab assignment #1 javascript file
-
+// Creates array of correct possible answers for each question
+var correctAnswers = ["yes", "y", "no", "n", "no", "n", "no", "n"];
+// Creates array of all user answers
 var answers = [];
+var correct = 0;
+var userName = prompt("What's your name?");
 //loop through each html id and prompt its question
 for (i = 1; i < 5; i++) {
   var question = document.getElementById(""+i+"");
 //store user input as lowercase via a prompt window
   answer = prompt(question.textContent).toLowerCase();
-//check to see if user input is acceptable per project specifications
-  if (answer != 'yes' && answer != 'no' && answer != 'y' && answer != 'n') {
-//alert user if response is invalid
-    alert("Hey, that's not a valid response!");
-//store value as false if response is invalid
-    answers[i-1] = false;
+//check to see if user input is correct
+  if (answer != correctAnswers[2*i-2] && answer != correctAnswers[2*i-1]) {
+//alert user if response is incorrect
+    alert("Sorry, that's not correct! Come on " + userName + "!");
+//store response value
+    answers[i-1] = answer;
   }
   else {
-//store response value if valid
+//alert user if response is correct
+    alert("Correct!");
+//store response value
     answers[i-1] = answer;
+    correct += 1;
   }
 }
