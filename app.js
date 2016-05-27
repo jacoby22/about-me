cdb3355b6b52063c8559eeb5e323e15325c1caaf
Nick Jacoby <nickjacoby22@gmail.com>
2016-05-26 19:54:37 -0700

    forced user input to be lower case each time looping through

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //lab assignment #1 javascript file
 // //loop through each html id and prompt its question
-for (i = 1; i < 5; i++) {
-  var h = document.getElementById(""+i+"");
+for (i = 1; i < 2; i++) {
+  var question = document.getElementById(""+i+"");
+  answer = prompt(question.textContent).toLowerCase();
   
 }
