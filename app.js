d1f4a2b3cd140c4c5f6dd0d5e79894dc5371a623
Nick Jacoby <nickjacoby22@gmail.com>
2016-05-26 20:06:52 -0700

    added if statement to check if user input was acceptable and stored response in new array if so

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 //lab assignment #1 javascript file
+
+var answers = [];
 // //loop through each html id and prompt its question
-for (i = 1; i < 2; i++) {
+for (i = 1; i < 5; i++) {
   var question = document.getElementById(""+i+"");
   answer = prompt(question.textContent).toLowerCase();
-  
+  if (answer != 'yes' && answer != 'no' && answer != 'y' && answer != 'n') {
+    alert("Hey, that's not a valid response!");
+    answers[i-1] = false;
+  }
+  else {
+    answers[i-1] = answer;
+  }
 }
