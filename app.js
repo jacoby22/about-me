e7f8f7d1b3d36560c1ed76a23434b3d5cf8e0753
Nick Jacoby <nickjacoby22@gmail.com>
2016-05-28 14:34:09 -0700

    Created nested array for each question and updated for loop to reference correct

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 //lab assignment #1 javascript file
 // Creates array of correct possible answers for each question
-var correctAnswers = ["yes", "y", "no", "n", "no", "n", "no", "n"];
+var correctAnswers = [
+                      ["yes", "y"],
+                      ["no", "n"],
+                      ["no", "n"],
+                      ["no", "n"]
+                                  ];
 // Creates array of all user answers
 var answers = [];
 var correct = 0;
 // prompt the user for his/her name for personalization later
 var userName = prompt("What's your name?");
 //loop through each html id and prompt its question
-for (var i = 1; i < 5; i++) {
+for (var i = 1; i < correctAnswers.length+1; i++) {
   var question = document.getElementById(""+i+"");
 //store user input as lowercase via a prompt window
   answer = prompt(question.textContent).toLowerCase();
 //check to see if user input is correct
-  if (answer != correctAnswers[2*i-2] && answer != correctAnswers[2*i-1]) {
+  if (correctAnswers[i-1].indexOf(answer) === -1) {
 //alert user if response is incorrect
     alert("Sorry, that's not correct! Come on " + userName + "!");
 //store response value
