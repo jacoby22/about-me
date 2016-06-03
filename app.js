5ff2074826fa4104983ce05103efb0fefb3bd548
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-02 18:54:56 -0700

    created function for asking yes or no questions called askYorNoQuestions

--- app.js
+++ app.js
@@ -20,64 +20,67 @@ function getUserName() {
 }
 getUserName();
 
-for (var i = 1; i < 5; i++) {
-  var question = document.getElementById(''+i+'');
-  //store user input as lowercase via a prompt window
-  answer = prompt(question.textContent).toLowerCase();
-//logs user answer for debugging code
-  console.log(answer);
-//logs array of possible answers for debugging code
-  console.log(correctAnswers[i]);
-//check to see if user input is one of possible correct answers
-  if (correctAnswers[i-1].indexOf(answer) === -1) {
-//alert user if response is incorrect
-    alert('Sorry, that\'s not correct! Come on ' + userName + '!');
-//store response value
-    answers.push(answer);
-  }
-  else {
-//alert user if response is correct
-    alert('Correct!');
-//store response value
-    answers[i-1] = answer;
-    correct += 1;
+function askYorNoQuestions() {
+  for (var i = 1; i < 5; i++) {
+    var question = document.getElementById('' + i + '');
+    //store user input as lowercase via a prompt window
+    answer = prompt(question.textContent).toLowerCase();
+  //logs user answer for debugging code
+    console.log(answer);
+  //logs array of possible answers for debugging code
+    console.log(correctAnswers[i]);
+  //check to see if user input is one of possible correct answers
+    if (correctAnswers[i - 1].indexOf(answer) === -1) {
+  //alert user if response is incorrect
+      alert('Sorry, that\'s not correct! Come on ' + userName + '!');
+  //store response value
+      answers.push(answer);
+    }
+    else {
+  //alert user if response is correct
+      alert('Correct!');
+  //store response value
+      answers[i - 1] = answer;
+      correct += 1;
+    }
   }
 }
+askYorNoQuestions();
 //variable for storing the allowable number of user guesses
 var allowedGuesses = 4;
 var question5 = document.getElementById(5);
-for (var i = 0; i < allowedGuesses; i++) {
+for (var j = 0; j < allowedGuesses; j++) {
 //cast user input into an integer for comparing against stored correct int value
   var userGuess = parseInt(prompt(question5.textContent));
   answers.push(userGuess);
 //check if user guess is equal to stored array value
   if (userGuess === correctAnswers[4][0]) {
-    alert('Correct! You have guessed the magic number.')
+    alert('Correct! You have guessed the magic number.');
     correct += 1;
     break;
 //console log the user's guess for easier debugging
-  console.log(userGuess);
+    console.log(userGuess);
   }
 //check if user guess is higher than stored correct int value
   else if (userGuess > correctAnswers[4][0]){
-    alert('Incorrect! You\'re guess was a little high. \nYou have ' + (allowedGuesses - (i+1)) + ' guesses remaining.');
+    alert('Incorrect! You\'re guess was a little high. \nYou have ' + (allowedGuesses - (j + 1)) + ' guesses remaining.');
   }
   else {
-    alert('Incorrect! You\'re guess was a little low. \nYou have ' + (allowedGuesses - (i+1)) + ' guesses remaining.');
+    alert('Incorrect! You\'re guess was a little low. \nYou have ' + (allowedGuesses - (j + 1)) + ' guesses remaining.');
   }
 }
 //store a new value for allowedGuesses for question 6
-var allowedGuesses = 6;
+var allowedGuesses2 = 6;
 var question6 = document.getElementById(6);
-for (var i = 0; i < allowedGuesses; i++) {
+for (var k = 0; k < allowedGuesses2; k++) {
 //get user input and force it to lower case to match array answers
-  var userGuess = prompt(question6.textContent).toLowerCase();
-  answers.push(userGuess);
-  if (correctAnswers[5].indexOf(userGuess) === -1) {
-    alert('Incorrect! I\'ve never been to ' + userGuess + '.\nYou have '+ (allowedGuesses - (i+1)) + ' guesses remaining.');
+  var userGuess2 = prompt(question6.textContent).toLowerCase();
+  answers.push(userGuess2);
+  if (correctAnswers[5].indexOf(userGuess2) === -1) {
+    alert('Incorrect! I\'ve never been to ' + userGuess2 + '.\nYou have ' + (allowedGuesses2 - (k + 1)) + ' guesses remaining.');
   }
   else {
-    alert('Correct! I have travelled to ' + userGuess + '.');
+    alert('Correct! I have travelled to ' + userGuess2 + '.');
     correct += 1;
     break;
   }
